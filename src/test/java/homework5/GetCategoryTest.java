5b6b3fcbcb002f639996f8045d59490352ff41fd
krutikov-alex <krutikov-alex@ya.ru>
2022-05-21 13:00:52 +0300

    Homework 5 in progress

---
+++ src/test/java/homework5/GetCategoryTest.java
@@ -0,0 +1,36 @@
+package homework5;
+
+import homework5.api.CategoryService;
+import homework5.dto.GetCategoryResponse;
+import homework5.utils.RetrofitUtils;
+import lombok.SneakyThrows;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import retrofit2.Response;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class GetCategoryTest {
+
+    static CategoryService categoryService;
+    @BeforeAll
+    static void beforeAll() {
+        categoryService = RetrofitUtils.getRetrofit().create(CategoryService.class);
+    }
+
+    @SneakyThrows
+    @Test
+    void getCategoryByIdPositiveTest() {
+        Response<GetCategoryResponse> response = categoryService.getCategory(2).execute();
+
+        assertThat(response.isSuccessful(), CoreMatchers.is(true));
+        assertThat(response.body().getId(), equalTo(2));
+        assertThat(response.body().getTitle(), equalTo("Electronic"));
+        response.body().getProducts().forEach(product ->
+                assertThat(product.getCategoryTitle(), equalTo("Electronic")));
+
+    }
+
+}
